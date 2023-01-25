d88cc48aa664cf08093fe3c7a551e07f7a166828
Lautaro Pereyra <hi@imlautaro.com>
2023-01-24 21:39:35 -0300

    add products to sitemap

--- src/server/routes/sitemap.xml.ts
+++ src/server/routes/sitemap.xml.ts
@@ -1,10 +1,26 @@
 import { SitemapStream, streamToPromise } from 'sitemap'
+import { serverSupabaseClient } from '#supabase/server'
 
-export default defineEventHandler(async () => {
+export default defineEventHandler(async event => {
 	const runtimeConfig = useRuntimeConfig()
+	const supabase = serverSupabaseClient(event)
 
 	const sitemap = new SitemapStream({
 		hostname: runtimeConfig.public.siteURL,
+	})
+
+	const { data: products } = await supabase
+		.from('products')
+		.select(
+			'id, name, price, image, visible, slug, parent:parent_id(name, price), category:category_id(name, slug)'
+		)
+		.eq('visible', 'true')
+
+	products?.forEach(product => {
+		sitemap.write({
+			// @ts-ignore
+			url: `/${product.category.slug}/${product.slug}`,
+		})
 	})
 
 	const pages = ['/']
